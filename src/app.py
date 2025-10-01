0f2ef5c055daa24fb960f55d49e6a01c714e93df
AdriARE <adrian.r.estevez@gmail.com>
2025-10-01 18:27:05 +0000

    Empezando proyecto

--- src/app.py
+++ src/app.py
@@ -6,3 +6,10 @@ import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
+#Paso 1
+resource_url = " https://en.wikipedia.org/wiki/List_of_most-streamed_songs_on_Spotify"
+response = requests.get(resource_url, time.sleep(3))
+
+#Paso 2
+response
